6a9d9d109a4cbb7f18325be1e2bdecbb9b1718ba
ZaidAguilar <azar990210@gmail.com>
2024-02-20 23:10:54 -0600

    commit inicial

---
+++ src/main/java/com/services/solutions/apibase/ApibaseApplication.java
@@ -0,0 +1,13 @@
+package com.services.solutions.apibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApibaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/services/solutions/apibase/ApibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.solutions.apibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
